f5e0fd461aae3e4bbd78cbd37b8bd4a54d9afaaf
Arlind-Salihu <arlindsalihu12@outlook.com>
2025-04-03 23:23:31 +0200

    Start of navbar structure and menu items

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import "../style/style.css";
+import logo from "../assets/mister_spex_logo.svg";
 
 const Home = () => {
   return (
-    <div>Home Page</div>
-  )
-}
+    <div className="body">
+      <div className="navbar">
+        <div className="nav-header">
+          Home
+          <img alt="" src={logo} width="200" />
+          <div className="nav-header-items">
+            <ul>
+              <li>1</li>
+              <li>2</li>
+              <li>3</li>
+              <li>4</li>
+            </ul>
+          </div>
+        </div>
+        <div className="nav-menu">
+          <ul>
+            <li>a</li>
+            <li>b</li>
+            <li>c</li>
+            <li>d</li>
+            <li>e</li>
+            <li>f</li>
+            <li>g</li>
+            <li>h</li>
+          </ul>
+        </div>
+      </div>
+      <div className="container"></div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
