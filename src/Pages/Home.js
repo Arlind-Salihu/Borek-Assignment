1e035bf6f7e758e22705950d1ffcfa818113658d
Arlind-Salihu <arlindsalihu12@outlook.com>
2025-04-05 14:10:46 +0200

    Added the steps section as per the Figma Desing

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,37 +1,67 @@
 import React from "react";
 import "../style/style.css";
-import logo from "../assets/mister_spex_logo.svg";
+import logo from "../assets/mister-spex-logo.svg";
 
 const Home = () => {
   return (
     <div className="body">
+      {/* Navbar */}
       <div className="navbar">
         <div className="nav-header">
-          Home
-          <img alt="" src={logo} width="200" />
+          <span className="nav-help">
+            <i class="bi bi-question-circle"></i>
+            Hilfe & Beratung
+          </span>
+          <img alt="logo" src={logo} className="nav-logo" />
           <div className="nav-header-items">
             <ul>
-              <li>1</li>
-              <li>2</li>
-              <li>3</li>
-              <li>4</li>
+              <li>
+                <i class="bi bi-search"></i>
+              </li>
+              <li>
+                <i class="bi bi-geo-alt"></i>
+              </li>
+              <li>
+                <i class="bi bi-person"></i>
+              </li>
+              <li>
+                <i class="bi bi-heart"></i>
+              </li>
+              <li>
+                <i class="bi bi-handbag"></i>
+              </li>
             </ul>
           </div>
         </div>
         <div className="nav-menu">
-          <ul>
-            <li>a</li>
-            <li>b</li>
-            <li>c</li>
-            <li>d</li>
-            <li>e</li>
-            <li>f</li>
-            <li>g</li>
-            <li>h</li>
-          </ul>
+            <a>Brillen</a>
+            <a>Sonnenbrillen</a>
+            <a>Kontaktlinsen</a>
+            <a>Marken</a>
+            <a>Boutique</a>
+            <a>Kostenloser Sehtest</a>
+            <a>Stores</a>
+            <a>Winter Deal</a>
         </div>
       </div>
-      <div className="container"></div>
+      {/* Container */}
+      <div className="container">
+        {/* Steps */}
+        <div class="stepper">
+          <div class="step active">
+            <div class="step-number">1</div>
+            <div class="step-label">Beitrag</div>
+          </div>
+          <div class="step">
+            <div class="step-number">2</div>
+            <div class="step-label">Antragsdaten</div>
+          </div>
+          <div class="step">
+            <div class="step-number">3</div>
+            <div class="step-label">Zusammenfassung</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
